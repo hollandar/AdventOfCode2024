89db7b2074b0b99bc2cfd1aafcaba43517982edd
Adrian Holland <hollandar@gmail.com>
2024-12-02 22:33:59 +1100

    Red Nosed Reports

---
+++ Command/Problems/2024/RedNosedReports.cs
@@ -0,0 +1,88 @@
+﻿using Command.Framework;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace Command.Problems._2024;
+
+public record Report(int[] levels);
+
+public partial class RedNosedReports : ProblemBase<int>
+{
+    List<Report> reports = new();
+
+    public RedNosedReports()
+    {
+    }
+
+    protected override void Line(string line)
+    {
+        var levels = line.Split(' ').Select(int.Parse).ToArray();
+        reports.Add(new Report(levels));
+    }
+
+    public bool IsSafe(int[] levels)
+    {
+        int[] directions = new int[levels.Length - 1];
+        int[] climbs = new int[levels.Length - 1];
+
+        for (int i = 0; i < levels.Length - 1; i++)
+        {
+            var climbValue = levels[i + 1] - levels[i];
+            directions[i] = climbValue switch
+            {
+                > 0 => 1,
+                < 0 => -1,
+                _ => 0
+            };
+            climbs[i] = Math.Abs(climbValue);
+        }
+
+        return climbs.All(c => c > 0 && c <= 3) && (directions.All(d => d == 1) || directions.All(d => d == -1));
+    }
+
+    public override int CalculateOne()
+    {
+        int count = 0;
+        foreach (var report in reports)
+        {
+            if (IsSafe(report.levels))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    
+    public override int CalculateTwo()
+    {
+        int count = 0;
+        foreach (var report in reports)
+        {
+            if (IsSafe(report.levels))
+            {
+                count++;
+                continue;
+            }
+
+            for (int i = 0; i < report.levels.Length; i++)
+            {
+                int j = i + 1;
+                int[] levels = (report.levels[..i]).Concat(report.levels[j..]).ToArray();
+                if (IsSafe(levels))
+                {
+                    count++;
+                    goto next;
+                }
+            }
+
+        next:
+            continue;
+        }
+
+        return count;
+    }
+
+
+}
--- Command/Program.cs
+++ Command/Program.cs
@@ -2,9 +2,10 @@
 using Command.Problems;
 
 //ProblemRunner<Command.Problems._2022.CalorieCounting, int>.Run("2022/CalorieCounting");
-ProblemRunner<Command.Problems._2022.RockPaperScissors, int>.Run("2022/RockPaperScissors");
+//ProblemRunner<Command.Problems._2022.RockPaperScissors, int>.Run("2022/RockPaperScissors");
 
 //ProblemRunner<Command.Problems._2023.Trebuchet, int>.Run("2023/Trebuchet");
 //ProblemRunner<Command.Problems._2023.CubeConundrum, int>.Run("2023/CubeConundrum");
 
 //ProblemRunner<Command.Problems._2024.HistorianHysteria, int>.Run("2024/HistorianHysteria");
+ProblemRunner<Command.Problems._2024.RedNosedReports, int>.Run("2024/RedNosedReports");
