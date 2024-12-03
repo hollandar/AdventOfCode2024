b9aa087236bc78eb236e06506837a17fe694f069
adrian holland <hollandar@gmail.com>
2024-12-03 21:30:34 +1100

    Mull it over

--- Command/Program.cs
+++ Command/Program.cs
@@ -8,4 +8,5 @@ using Command.Problems;
 //ProblemRunner<Command.Problems._2023.CubeConundrum, int>.Run("2023/CubeConundrum");
 
 //ProblemRunner<Command.Problems._2024.HistorianHysteria, int>.Run("2024/HistorianHysteria");
-ProblemRunner<Command.Problems._2024.RedNosedReports, int>.Run("2024/RedNosedReports");
+//ProblemRunner<Command.Problems._2024.RedNosedReports, int>.Run("2024/RedNosedReports");
+ProblemRunner<Command.Problems._2024.MullItOver, int>.Run("2024/MullItOver");
